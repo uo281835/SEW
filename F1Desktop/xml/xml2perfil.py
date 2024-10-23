774b2b40dde5a49518f824227292405ba3d23087
UO281835 <uo281835@uniovi.es>
2024-10-23 12:56:38 +0200

    Se han creado las plantillas de los conversores a kml y svg

---
+++ F1Desktop/xml/xml2perfil.py
@@ -0,0 +1,34 @@
+import xml.etree.ElementTree as ET
+
+def escrituraPrologo():
+    result = ""
+
+    return result
+
+def escrituraEpilogo():
+    result =""
+
+    return result
+
+def escrituraRuta():
+    lista = [1,2,4,5]
+    result =""
+    for i in lista:
+        result+=escrituraPunto(i)
+    return result
+
+def escrituraPunto(punto):
+    result =""
+
+    return result
+
+def crearSVG():
+    result =""
+    result+=escrituraPrologo()
+    result+=escrituraRuta()
+    result+=escrituraEpilogo()
+    return result
+
+def main():
+    resultado = crearKML()
+    print(resultado)
