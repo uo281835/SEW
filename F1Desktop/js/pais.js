991dcc59bc8320d911bf7f59ac649353b310a92c
UO281835 <uo281835@uniovi.es>
2024-11-13 11:57:35 +0100

    Primera versión de la meteorología, falta poner que sean varios días

--- F1Desktop/js/pais.js
+++ F1Desktop/js/pais.js
@@ -5,11 +5,11 @@ class Pais{
         this.circuito = circuito;
         this.apikey = "47b790fd0fc41878c80c57c9846132cb";
         this.ciudad = "";
-        this.codigoPais = "ES";
+        this.codigoPais = "&JP";
         this.unidades = "&units=metric";
         this.idioma = "&lang=es";
         this.url = "http://api.openweathermap.org/data/2.5/weather?q=" + this.ciudad + "," + this.codigoPais + this.unidades + this.idioma + "&APPID=" + this.apikey;
-        this.correcto = "¡Todo correcto! JSON recibido de <a href='http://openweathermap.org'>OpenWeatherMap</a>"  
+        this.correcto = "¡Todo correcto! Datos recibidos de <a href='http://openweathermap.org'>OpenWeatherMap</a>"  
     }
 
     rellenarDatos(poblacion, formaGobierno, lineaMeta, religionMayoritaria) {
@@ -42,37 +42,23 @@ class Pais{
     }
     obtenerDatos(ciudad){
         this.ciudad = ciudad;
-        this.url = "http://api.openweathermap.org/data/2.5/weather?q=" + this.ciudad + "," + this.codigoPais + this.unidades + this.idioma + "&APPID=" + this.apikey;
+        this.url = "http://api.openweathermap.org/data/2.5/weather?q=" + this.ciudad+"&mode=xml"+ this.unidades + this.idioma + "&APPID=" + this.apikey;
         $.ajax({
-            dataType: "json",
+            dataType: "xml",
             url: this.url,
             method: 'GET',
             success: function(data){
-                $("pre").text(JSON.stringify(data, null, 2)); //muestra el json en un elemento pre
-
-                var string = "<ul><li>Ciudad: "+data.name + "</li>";
-                string+="<li> País: "+data.sys.country+"</li>";
-                string+="<li> Latitud: "+data.coord.lat+"</li>";
-                string+="<li> Longitud: "+data.coord.lon+"</li>";
-                string+="<li> Temperatura: "+data.main.temp+"</li>";
-                string+="<li> Temperatura maxima: "+data.main.temp_max+"</li>";
-                string+="<li> Temperatura minima: "+data.main.temp_min+"</li>";
-                string+="<li> Presión: "+data.sys.pressure+"</li>";
-                string+="<li> Humedad: "+data.sys.humidity+"</li>";
-                string+= "<li>Amanece a las: " + new Date(data.sys.sunrise *1000).toLocaleTimeString() + "</li>";
-                string+="<li>Oscurece a las: " + new Date(data.sys.sunset *1000).toLocaleTimeString() + "</li>";
-                string += "<li>Dirección del viento: " + data.wind.deg + " grados</li>";
-                string += "<li>Velocidad del viento: " + data.wind.speed + " metros/segundo</li>";
-                string += "<li>Hora de la medida: " + new Date(data.dt *1000).toLocaleTimeString() + "</li>";
-                string += "<li>Fecha de la medida: " + new Date(data.dt *1000).toLocaleDateString() + "</li>";
-                string += "<li>Descripción: " + data.weather[0].description + "</li>";
-                string += "<li>Visibilidad: " + data.visibility + " metros</li>";
-                string += "<li>Nubosidad: " + data.clouds.all + " %</li></ul>";
-                string += "<img src = '"+"http://openweathermap.org/img/w/"+data.weather[0].icon+".png'"+"/>";
-                $("p").html(string);
+               
+                var string = "<article><h3>Ciudad: "+$('city',data).attr("name") + "</h3>";
+                string+="<p> Temperatura maxima: "+$('temperature',data).attr("max")+"</p>";
+                string+="<p> Temperatura minima: "+$('temperature',data).attr("min")+"</p>";
+                string+="<p> Humedad: "+$('humidity',data).attr("value")+"</p>";
+                string += "<p>Precipitación: " + $('precipitation',data).attr("value") + "</p>";
+                string += "<img src = '"+"http://openweathermap.org/img/w/"+$('weather',data).attr("icon")+".png'"+"/></article>";
+                $("article").html(string);
             },
             error:function(){
-                $("h3").html("¡Tenemos problemas! No puedo obtener JSON de <a href='http://openweathermap.org'>OpenWeatherMap</a>"); 
+                $("h3").html("¡Tenemos problemas! No puedo obtener de <a href='http://openweathermap.org'>OpenWeatherMap</a>"); 
                 $("h4").remove();
                 $("pre").remove();
                 $("p").remove();
@@ -88,13 +74,4 @@ class Pais{
 }
 
 
-var pais = new Pais("Japón", "Tokio", "Suzuka");
-pais.rellenarDatos("123719238", "Monarquía Parlamentaria", "34.8429239,136.5402045,46.8373725", "Sintoísmo");
-
-console.log(document.querySelector("body"))
-//document.querySelector("body main").write("<section>");
-document.querySelector("body main h3").innerHTML = ("<h3>"+pais.getNombre()+"</h3>");
-document.querySelector("body main h4").innerHTML=("<h4>"+pais.getCapital()+"</h4>");
-document.querySelector("body main ul").innerHTML = (pais.getInfoSecundaria());
-pais.escribirCoordenadas();
 
