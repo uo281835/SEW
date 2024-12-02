df7aef9a874916204f6eef81ed5277e81e35bf36
Juanakin18 <juaniteje@gmail.com>
2024-12-02 15:46:14 +0100

    Agenda hecha

--- F1Desktop/js/agenda.js
+++ F1Desktop/js/agenda.js
@@ -2,4 +2,61 @@ class Agenda{
     constructor(){
         this.url = "https://api.jolpi.ca/ergast/f1/current";
     }
-}
+
+    getInfo(){
+
+        $.ajax({
+            dataType: "json",
+            url: this.url,
+            method: 'GET',
+            success: function(data1){
+                $("section").empty()
+                var races = (data1.MRData.RaceTable.Races)
+                for(var i=0; i<races.length; i++){
+                    var race = races[i];
+                    var raceName = race.raceName;
+                    var circuitName = race.Circuit.circuitName;
+                    var lat = race.Circuit.Location.lat
+                    var long = race.Circuit.Location.long;
+                    var fecha = race.date;
+                    var hora = race.time;
+
+                    console.log(raceName)
+                    console.log(circuitName)
+                    console.log(lat, long)
+                    console.log(fecha, hora)
+
+                        
+                    //PINTADO
+                    var article = document.createElement("article");
+
+                    var h3 = document.createElement("h3");
+                    $(h3).text(raceName);
+                    $(article).append(h3);
+
+                    var h4 = document.createElement("h4");
+                    $(h4).text(circuitName);
+                    $(article).append(h4);
+
+                    var pCoordenadas = document.createElement("p");
+                    $(pCoordenadas).text("Coordenadas: Lat"+lat+"long:"+long);
+                    $(article).append(pCoordenadas);
+
+                    var pFechaYHora = document.createElement("p");
+                    $(pFechaYHora).text("Fecha:"+fecha+" Hora: "+hora);
+                    $(article).append(pFechaYHora);
+
+                    $("main section").append(article);
+
+
+                }
+              
+      
+            },
+            error:function(){
+              $("h2:contains(Tiempo)").after("<p>¡Tenemos problemas! No puedo obtener JSON de OpenWeatherMap</p>");
+            }
+      
+          });
+        }
+    }
