d98227f1a29ae2381782a9aabf8bc883ae341ba0
Juanakin18 <juaniteje@gmail.com>
2024-12-02 13:59:20 +0100

    Meteorología hecha

--- F1Desktop/js/pais.js
+++ F1Desktop/js/pais.js
@@ -3,12 +3,14 @@ class Pais{
         this.nombre = nombre;
         this.capital = capital;
         this.circuito = circuito;
-        this.apikey = "47b790fd0fc41878c80c57c9846132cb";
+        this.apikey = "4fbf610da01d3782582a723f7a6f9156";
+        this.lat=34.8429239;
+        this.lon=136.5402045;
         this.ciudad = "";
         this.codigoPais = "&JP";
         this.unidades = "&units=metric";
         this.idioma = "&lang=es";
-        this.url = "http://api.openweathermap.org/data/2.5/weather?q=" + this.ciudad + "," + this.codigoPais + this.unidades + this.idioma + "&APPID=" + this.apikey;
+        this.url = "http://api.openweathermap.org/data/2.5/forecast?lat=" + this.lat + "&lon=" + this.lon+"&mode=xml&appid=" + this.apikey;
         this.correcto = "¡Todo correcto! Datos recibidos de <a href='http://openweathermap.org'>OpenWeatherMap</a>"  
     }
 
@@ -42,20 +44,104 @@ class Pais{
     }
     obtenerDatos(ciudad){
         this.ciudad = ciudad;
-        this.url = "http://api.openweathermap.org/data/2.5/weather?q=" + this.ciudad+"&mode=xml"+ this.unidades + this.idioma + "&APPID=" + this.apikey;
         $.ajax({
             dataType: "xml",
             url: this.url,
             method: 'GET',
             success: function(data){
+                var arrayData = $('forecast',data).children();
+                var tmax = 0;
+                var tmin = 500;
+                var acumuladoH =0;
+                var nH =0;
+                var icono1 ="";
+                var lluvia=0;
+
+                for(var i =0; i<arrayData.length; i++){
+                    //Acceso a datos
+                    var datos = arrayData[i];
+
+                    var temperatureMin = $('temperature',datos).attr('min');
+                    var temperatureMax = $('temperature',datos).attr('max');
+
+                    var icono = $('symbol', datos);
+                    var humedad = new Number($('humidity',datos).attr("value"));
+
+                    var precip = $('precipitation', datos)
+                    
+
+                    var from = $(datos).attr('from').substring(0,10)
+                    var to = $(datos).attr('to').substring(0,10)
+
+                    //Procesado de datos
+                    if(temperatureMin<tmin)
+                        tmin=temperatureMin;
+
+                    if(temperatureMax>tmax)
+                        tmax=temperatureMax;
+
+                    icono1 = icono;
+
+                    acumuladoH+=humedad;
+                    nH+=1;
+
+                    if($(precip).attr('probability')>0){
+                        lluvia+=$(precip).attr('value')
+                    }
+                    
+
+                    if(from!==to){
+                        //Pintado
+                        var humedadPromedio = acumuladoH/nH;
+
+                        
+
+                        var article = document.createElement("article");
+
+                        var h3 = document.createElement("h3");
+                        $(h3).text("Suzuka");
+                        $(article).append(h3);
+
+                        var h4 = document.createElement("h4");
+                        $(h4).text(from);
+                        $(article).append(h4);
+
+                        var img = document.createElement("img");
+                        $(img).attr("src","http://openweathermap.org/img/w/"+$(icono1).attr("var")+".png");
+                        $(img).attr("alt",+$(icono1).attr("name"));
+                        $(article).append(img);
+
+                        var pTMax = document.createElement("p");
+                        $(pTMax).text("Temperatura Máxima:"+(tmax-273)+"ºC");
+                        $(article).append(pTMax);
+
+                        var pTMin = document.createElement("p");
+                        $(pTMin).text("Temperatura Mínima:"+(tmin-273)+"ºC");
+                        $(article).append(pTMin);
+
+                        var ptHumid = document.createElement("p");
+                        $(ptHumid).text("Humedad:"+(humedadPromedio)+"%");
+                        $(article).append(ptHumid);
+
+                        var ptLluvia = document.createElement("p");
+                        $(ptLluvia).text("Lluvia:"+(lluvia));
+                        $(article).append(ptLluvia);
+
+                        var articulo =""
+                        articulo+="<p> Humedad: "+humedadPromedio+"</p>";
+                        articulo += "<p>Precipitación: " + lluvia + "</p></article>";
+                        $("main section")[1].append(article);
+
+                        //Reinicio de variables
+                        tmax =0;
+                        tmin=500;
+                        acumuladoH =0;
+                        nH =0;
+                        icono1 ="";
+                        lluvia=0;
+                    }
+                }
                
-                var string = "<article><h3>Ciudad: "+$('city',data).attr("name") + "</h3>";
-                string += "<img src = '"+"http://openweathermap.org/img/w/"+$('weather',data).attr("icon")+".png'"+"alt='iconoTiempo'/></article>";
-                string+="<p> Temperatura maxima: "+$('temperature',data).attr("max")+" ºC</p>";
-                string+="<p> Temperatura minima: "+$('temperature',data).attr("min")+" ºC</p>";
-                string+="<p> Humedad: "+$('humidity',data).attr("value")+"</p>";
-                string += "<p>Precipitación: " + $('precipitation',data).attr("value") + "</p>";
-                $("article").html(string);
             },
             error:function(){
                 $("h3").html("¡Tenemos problemas! No puedo obtener de <a href='http://openweathermap.org'>OpenWeatherMap</a>"); 
