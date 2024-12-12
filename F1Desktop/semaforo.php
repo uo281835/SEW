c36e0e8870875ee614b60d1206955e147c8e8f7e
Juanakin18 <juaniteje@gmail.com>
2024-12-12 20:05:35 +0100

    Arreglo de rutas

--- F1Desktop/semaforo.php
+++ F1Desktop/semaforo.php
@@ -13,7 +13,7 @@
     <link rel="stylesheet" type="text/css" href="estilo/semaforo.css" />
     <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
 
-    <script src="/F1Desktop/js/semaforo.js"></script>
+    <script src="js/semaforo.js"></script>
 
     <?php
 
